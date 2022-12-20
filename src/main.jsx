f6f8eb99385882643a7e98134a89bec177ad5d97
Huevaldinho <47650141+Huevaldinho@users.noreply.github.com>
2022-12-20 10:39:56 -0600

    Clone proyecto en laptop

--- src/main.jsx
+++ src/main.jsx
@@ -11,3 +11,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </TaskContextProvider>
   </React.StrictMode>,
 )
+
+/*
+Pasos para instalar este proyecto en otra pc
+1) Clonar el repo: git clone https://github.com/Huevaldinho/learning-react.git
+2) Instalar npm modules: Abrir el proyecto: npm install
+3) Listo: npm run dev
+*/
