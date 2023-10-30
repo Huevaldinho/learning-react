cdedd6ac25e92c62683f5ec259545c040d148c85
Huevaldinho <felipeobando@estudiantec.cr>
2023-10-29 19:33:59 -0600

    Cambio de color

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,7 +2,7 @@ import TaskForm from "../components/tasks/TaskForm";
 import TaskList from "../components/tasks/TaskList";
 function MainPage() {
   return (
-    <main className="bg-red-600 h-screen">
+    <main className="bg-zinc-900 h-screen">
       <div className="container mx-auto p-10">
         <TaskForm />
         <TaskList />
